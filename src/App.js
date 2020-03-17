a46189960f1893321dcd5a1d83ad79198552b590
osvalds <osvalds.neiders@gmail.com>
2020-03-17 17:04:06 +0200

    Let manually toggle state of a cell

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.scss';
 
 const [ROWS, COLS] = [20, 20];
 
-function Cell({cell}) {
+function Cell({cell, coord, toggleCell}) {
+
     return (
-        <div className="cell">
+        <div
+            onClick={e => toggleCell(coord)}
+            className="cell">
             {cell}
         </div>
     )
 }
 
-function Row({row}) {
+function Row({row, y, toggleCell}) {
     return (
         <div className="row">
-            {row.map((cell, index) => <Cell key={index} cell={cell}/>)}
+            {row.map((cell, index) => <Cell key={index} cell={cell} coord={{x: index, y: y}} toggleCell={toggleCell}/>)}
         </div>
     )
 }
 
 function App() {
-    let board = Array(ROWS).fill().map(() => Array(COLS).fill(0));
+    let startBoard = Array(ROWS).fill().map(() => Array(COLS).fill(0));
+
+    const [board, setBoard] = useState(startBoard);
+
+    let toggleCell = ({x, y}) => {
+        let newBoard = [...board].map(arr => arr.slice(0));
+        newBoard[y][x] = board[y][x] === 0 ? 1 : 0;
+        setBoard(newBoard);
+    };
 
     return (
         <div className="board">
-            {board.map((row, index) => <Row key={index} row={row}/>)}
+            {board.map((row, index) => <Row key={index} row={row} y={index} toggleCell={toggleCell}/>)}
         </div>
     );
 }
