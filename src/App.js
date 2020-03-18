4f47b063178aae2cc595e827a69e4f7f5a87d8e9
osvalds <osvalds.neiders@gmail.com>
2020-03-18 21:15:04 +0200

    Change implementation to use 1D array

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React, {useState} from 'react';
+import React from 'react';
 import useInterval from "./useInterval";
 import './App.scss';
 import useWindowSize from "./useWindowSize";
+import useBoard from "./useBoard";
 
 const INTERVAL = 100;
 
-function Cell({cell, coord, toggleCell}) {
+function Cell({index, cell, toggleCell}) {
     return (
         <div
-            onClick={e => toggleCell(coord)}
-            onMouseOver={e => {
+            onClick={() => toggleCell(index)}
+            onMouseEnter={(e) => {
                 if (e.buttons === 1 || e.buttons === 3) {
-                    toggleCell(coord)
+                    toggleCell(index)
                 }
             }}
             className={`cell cell--${cell}`}>
@@ -19,97 +20,28 @@ function Cell({cell, coord, toggleCell}) {
     )
 }
 
-function Row({row, y, toggleCell}) {
+function Board({board, toggleCell}) {
     return (
-        <div className="row">
-            {row.map((cell, index) => <Cell key={index} cell={cell} coord={{x: index, y: y}} toggleCell={toggleCell}/>)}
+        <div className="board">
+            {board.map((cell, index) => {
+                return (
+                    <Cell key={index} index={index} cell={cell} toggleCell={toggleCell}/>
+                )
+            })}
         </div>
     )
 }
 
-function mod(x, m) {
-    return (x % m + m) % m;
-}
+function BoardWrapper({COLS, ROWS, seed}) {
 
-// this will let wrap the neighbor detection to wrap around
-const calcCoord = (y, yd, MAX) => {
-    return mod(y + yd, MAX);
-};
-
-const countNeighbors = ({x, y}, board, ROWS, COLS) => {
-    // hard coded for perf reasons
-    const neighborDiff = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
-
-    let neighborCount = 0;
-
-    for (let [xd, yd] of neighborDiff) {
-        if (board[calcCoord(y, yd, ROWS)][calcCoord(x, xd, COLS)] === 1) {
-            neighborCount += 1;
-        }
-    }
-    return neighborCount;
-};
-
-const isDesolate = (board) => {
-    for (let y = 0; y < board.length; y++) {
-        for (let x = 0; x < board[y].length; x++) {
-            if (board[y][x] === 1) {
-                return false;
-            }
-        }
-    }
-    return true;
-};
-
-function App() {
-
-    const windowSize = useWindowSize();
-    let ROWS = Math.floor((windowSize.height + 1) / (20 + 1));
-    let COLS = Math.floor((windowSize.width + 1) / (20 + 1));
-    let startBoard = Array(ROWS).fill().map(() => Array(COLS).fill(0));
-
-    const [board, setBoard] = useState(startBoard);
-    const [isRunning, setIsRunning] = useState(false);
-
-    let toggleCell = (coord) => {
-        const {x, y} = coord;
-        let newBoard = [...board].map(arr => arr.slice(0));
-        newBoard[y][x] = board[y][x] === 0 ? 1 : 0;
-        setBoard(newBoard);
-    };
-
-    let advanceBoard = () => {
-        // const currentBoard = [...board].map(arr => arr.slice(0));
-        let newBoard = [...board].map(arr => arr.slice(0));
-
-        for (let y = 0; y < board.length; y++) {
-            for (let x = 0; x < board[y].length; x++) {
-                const neighborCount = countNeighbors({x, y}, board, ROWS, COLS);
-
-                if (board[y][x] === 1) {
-                    if (neighborCount < 2 || neighborCount > 3) {
-                        newBoard[y][x] = 0;
-                    }
-                } else {
-                    if (neighborCount === 3) {
-                        newBoard[y][x] = 1;
-                    }
-                }
-            }
-        }
-        // console.log("is desolate:", isDesolate(newBoard))
-
-        setBoard(newBoard);
-        // if (isDesolate(newBoard)) {
-        //     setIsRunning(false);
-        // }
-    };
+    const [board, setBoard, isRunning, setIsRunning, toggleCell, advanceBoard] = useBoard(ROWS, COLS, seed);
 
     useInterval(advanceBoard, isRunning ? INTERVAL : null);
 
     return (
-        <div className="board">
-            {board.map((row, index) => <Row key={index} row={row} y={index} toggleCell={toggleCell}/>)}
+        <div>
+            <Board board={board} COLS={COLS} toggleCell={toggleCell}/>
+
             <div className="board__controls">
                 <button onClick={advanceBoard}>
                     Neeeext!
@@ -119,6 +51,19 @@ function App() {
                 </button>
             </div>
         </div>
+    )
+}
+
+function App() {
+    const windowSize = useWindowSize();
+    const gridGap = 1;
+    let ROWS = Math.floor((windowSize.height + gridGap) / (20 + gridGap));
+    let COLS = Math.floor((windowSize.width + gridGap) / (20 + gridGap));
+
+    const seed = Array(ROWS * COLS).fill(0);
+
+    return (
+        <BoardWrapper ROWS={ROWS} COLS={COLS} seed={seed}/>
     );
 }
 
---
+++ src/useBoard.js
@@ -0,0 +1,79 @@
+import {useState} from "react";
+
+const isDesolate = (board) => {
+    board.every((cell) => cell === 0)
+};
+
+const indexToCoord = (index, cols) => {
+    return {
+        x: index % cols,
+        y: Math.floor(index / cols)
+    }
+};
+
+const coordToIndex = ({x, y}, cols) => {
+    return y * cols + x;
+};
+
+const mod = (x, m) => {
+    return (x % m + m) % m;
+};
+
+// this will let wrap the neighbor detection to wrap around
+const calcCoord = (y, yd, MAX) => {
+    return mod(y + yd, MAX);
+};
+
+const countNeighbors = ({x, y}, board, ROWS, COLS) => {
+    // hard coded for perf reasons
+    const neighborDiff = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+
+    let neighborCount = 0;
+
+    for (let [xd, yd] of neighborDiff) {
+        const xCoord2D = calcCoord(x, xd, COLS);
+        const yCoord2D = calcCoord(y, yd, COLS);
+
+        if (board[coordToIndex({x: xCoord2D, y: yCoord2D}, COLS)] === 1) {
+            neighborCount += 1;
+        }
+    }
+    return neighborCount;
+};
+
+export default function useBoard(rows, cols, seed) {
+
+    const [board, setBoard] = useState(seed);
+    const [isRunning, setIsRunning] = useState(false);
+
+    let toggleCell = (clickedIndex) => {
+        let newBoard = [...board];
+        newBoard[clickedIndex] = board[clickedIndex] === 0 ? 1 : 0;
+        setBoard(newBoard);
+    };
+    const advanceBoard = () => {
+        let newBoard = [...board];
+
+        for (let i = 0, boardLength = board.length; i < boardLength; i++) {
+            const coord = indexToCoord(i, cols);
+            const neighborCount = countNeighbors(coord, board, rows, cols);
+
+            if (board[i] === 1) {
+                if (neighborCount < 2 || neighborCount > 3) {
+                    newBoard[i] = 0;
+                }
+            } else {
+                if (neighborCount === 3) {
+                    newBoard[i] = 1;
+                }
+            }
+        }
+
+        setBoard(newBoard);
+        if (isDesolate(newBoard)) {
+            setIsRunning(false);
+        }
+    };
+
+    return [board, setBoard, isRunning, setIsRunning, toggleCell, advanceBoard]
+}
