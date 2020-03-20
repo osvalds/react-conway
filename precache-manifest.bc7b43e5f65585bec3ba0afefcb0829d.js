7d99c2195ac3801f32ba99abe015b56ab0f9219b
osvalds <osvalds.neiders@gmail.com>
2020-03-20 21:54:40 +0200

    Updates

---
+++ precache-manifest.bc7b43e5f65585bec3ba0afefcb0829d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8758801606709af56f69e0599c4e1bf",
+    "url": "/react-conway/index.html"
+  },
+  {
+    "revision": "b53f6b3356e9fd977cc6",
+    "url": "/react-conway/static/css/main.90fc98e9.chunk.css"
+  },
+  {
+    "revision": "094160946878808eed90",
+    "url": "/react-conway/static/js/2.5a885246.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-conway/static/js/2.5a885246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53f6b3356e9fd977cc6",
+    "url": "/react-conway/static/js/main.6f02f909.chunk.js"
+  },
+  {
+    "revision": "6b1ff4acb4429b462248",
+    "url": "/react-conway/static/js/runtime-main.9d00aa68.js"
+  }
+]);
