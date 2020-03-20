a26cfd001a0bc9e4e1af45b6c4a13ade8d77d23e
osvalds <osvalds.neiders@gmail.com>
2020-03-20 20:16:37 +0200

    Add shortcuts for every option

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, {useCallback, useEffect} from "react";
 import {BrushSelector, getBrush, rotateBrush90deg} from "./Brushes";
 
 export default function Controls({advanceBoard, toggleIsRunning, isRunning, setIsRunning, setBoard, board, setSelectedBrushWrapper, selectedBrush}) {
+
+    const memoBoardReset = useCallback((event) => {
+        if (event.code === "KeyE") {
+            setIsRunning(false);
+            setBoard([...board].fill(0))
+        }
+    }, [setIsRunning, setBoard, board]);
+
+    useEffect(() => {
+        document.addEventListener("keydown", memoBoardReset, false);
+        return () => {
+            document.removeEventListener("keydown", memoBoardReset, false)
+        };
+    }, [memoBoardReset]);
+
+
+    const memoNext = useCallback((event) => {
+        if (event.code === "KeyW") {
+            setIsRunning(false);
+            advanceBoard();
+        }
+    }, [setIsRunning, advanceBoard]);
+
+    useEffect(() => {
+        document.addEventListener("keydown", memoNext, false);
+        return () => {
+            document.removeEventListener("keydown", memoNext, false)
+        };
+    }, [memoNext]);
+
     return (
         <div className="controls">
             <button
@@ -10,29 +40,29 @@ export default function Controls({advanceBoard, toggleIsRunning, isRunning, setI
                     setIsRunning(false);
                     advanceBoard()
                 }}>
-                Next
+                Next [<span>⇧</span>+w]
             </button>
             <button
                 className="button"
                 onClick={toggleIsRunning}>
-                {isRunning ? "Stop" : "Start"}
+                {isRunning ? "Stop [space]" : "Start [space]"}
             </button>
             <button
                 className="button"
-                onClick={() => {
-                    setIsRunning(false);
-                    setBoard([...board].fill(0))
-                }}>
-                Reset
+                onClick={memoBoardReset}>
+                Reset [<span>⇧</span>+e]
             </button>
-            <BrushSelector onChange={e => setSelectedBrushWrapper(getBrush((e.target.value)))}
+            <BrushSelector onChange={e => {
+                setSelectedBrushWrapper(getBrush((e.target.value)))
+                e.target.blur()
+            }}
                            selectedBrush={selectedBrush}/>
             <button
                 className="button"
                 onClick={() => {
                     setSelectedBrushWrapper(rotateBrush90deg(selectedBrush))
                 }}>
-                Rotate 90deg (shift + r)
+                Rotate 90deg [<span>⇧</span>+r]
             </button>
         </div>
     );
